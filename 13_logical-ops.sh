4fa5b16fd49941a6e904963335b0e0d68a7cab03
Sadique Iqbal <sadiqueiqbal28@gmail.com>
2025-06-20 12:20:13 +0530

    Fixed logical operator code

--- 13_logical-ops.sh
+++ 13_logical-ops.sh
@@ -4,9 +4,9 @@ read -p "Enter your age: " age
 read -p "Enter Your nationality: " nation
 nationality=${nation,,}
 
-if [[ $age -ge 18 && $nationality -eq "indian" ]]
+if [[ $age -ge 18 ]] && [[ $nationality -eq "indian" ]]
 then
-	echo "You can vote"
+	echo "You can vote because your age is $age and nationality is ${nationality^^}"
 else
 	echo "You can't vote"
 fi
